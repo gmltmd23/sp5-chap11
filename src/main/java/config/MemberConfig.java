d67784e3214b31a6fa346d7248abe6f7fef5e7b9
gmltmd23 <dbsgmltmd23@naver.com>
2021-08-21 00:17:59 +0900

    [SpringStudy] 11-6 커맨드 객체를 이용해서 요청 파라미터 사용하기 (2021/08/21)

--- src/main/java/config/MemberConfig.java
+++ src/main/java/config/MemberConfig.java
@@ -15,12 +15,13 @@ public class MemberConfig {
     @Bean
     public DataSource dataSource() {
         DataSource ds = new DataSource();
-        ds.setDriverClassName("com.mysql.jdbc.Driver");
-        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8");
+        ds.setDriverClassName("com.mysql.cj.jdbc.Driver");
+        ds.setUrl("jdbc:mysql://localhost/spring5fs?characterEncoding=utf8&serverTimezone=UTC");
         ds.setUsername("spring5");
         ds.setPassword("spring5");
         ds.setInitialSize(2);
-        ds.setMaxActive(10);
+        ds.setMaxActive(20);
+        ds.setMaxIdle(20);
         ds.setTestWhileIdle(true);
         ds.setMinEvictableIdleTimeMillis(60000 * 3);
         ds.setTimeBetweenEvictionRunsMillis(10 * 1000);
---
+++ src/main/webapp/WEB-INF/view/register/step3.jsp
@@ -0,0 +1,19 @@
+<%@ page contentType="text/html; charset=utf-8" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--
+  Created by IntelliJ IDEA.
+  User: Youn
+  Date: 2021-08-20
+  Time: 오후 11:50
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html; charset=utf-8" language="java" %>
+<html>
+<head>
+    <title>회원가입</title>
+</head>
+<body>
+    <p>회원가입을 완료했습니다.</p>
+    <p><a href="<c:url value='/'/>">[첫 화면 이동]</a></p>
+</body>
+</html>
